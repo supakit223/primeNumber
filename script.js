b029ddb4effd486bede95b103c79046ea8878796
supakit223 <supakit223@gmail.com>
2021-10-16 17:20:35 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function resetPage() {
+    // window.location.reload()
+    setTimeout(function(){location.reload()}, 100)
+}
+const inpObj = document.getElementById("sum")
+if (!inpObj.checkValidity()) {
+    document.getElementById("alert").classList.remove("invisible")
+    document.getElementById("alert").classList.add("visible")
+    
+    document.getElementById("output1").classList.remove("visible")
+    document.getElementById("output1").classList.add("invisible")
+    document.getElementById("output2").classList.remove("visible")
+    document.getElementById("output2").classList.add("invisible")
+
+    document.getElementById("alert").innerHTML = inpObj.validationMessage        
+}else if(count == 0) {           
+    document.getElementById("output1").classList.remove("invisible")
+    document.getElementById("output1").classList.add("visible")
+    document.getElementById("output2").classList.remove("visible")
+    document.getElementById("output2").classList.add("invisible")
+    document.getElementById("alert").classList.remove("visible")
+    document.getElementById("alert").classList.add("invisible")
+
+    document.getElementById("output1").innerHTML = `ไม่มีชุดคำตอบที่สอดคล้อง`       
+}else{
+    document.getElementById("output1").classList.remove("invisible")
+    document.getElementById("output1").classList.add("visible")
+    document.getElementById("output2").classList.remove("invisible")
+    document.getElementById("output2").classList.add("visible")
+    document.getElementById("alert").classList.remove("visible")
+    document.getElementById("alert").classList.add("invisible")
+
+    document.getElementById("output1").innerHTML = `มี ${count} ชุดคำตอบ ดังนี้`
+    document.getElementById("output2").innerHTML = number
+}    
