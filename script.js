630a6258c96d518264232f5f1162261ca6a33c8a
supakit223 <supakit223@gmail.com>
2021-10-17 21:41:10 +0700

    ok

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-function cal(){
+function cal1(){
     const input1 = document.getElementById("input1").value
     let count = 0
     let output1 = ""            
-    for(i=1 ; i<=input1 ; i++){       
+    for(let i=1 ; i<=input1 ; i++){       
         if (input1%i == 0) {
-            count++
+            count++            
             if (i < input1) {
-                output1 = output1 + ` ${i} , `
+                output1 = output1 + ` ${i} ,`
             } else {
                 output1 = output1 + ` ${i}`
             }            
         }        
     }         
-    const inpObj = document.getElementById("input1")
-    if (!inpObj.checkValidity()) {
-        document.getElementById("alert").classList.remove("invisible")
-        document.getElementById("alert").classList.add("visible")
+    const inpObj1 = document.getElementById("input1")
+    if (!inpObj1.checkValidity()) {
+        document.getElementById("alert1").classList.remove("invisible")
+        document.getElementById("alert1").classList.add("visible")
+        document.getElementById("alert2").classList.remove("visible")
+        document.getElementById("alert2").classList.add("invisible")
         document.getElementById("output1").classList.remove("visible")
         document.getElementById("output1").classList.add("invisible")   
 
-        document.getElementById("alert").innerHTML = inpObj.validationMessage         
+        document.getElementById("alert1").innerHTML = inpObj1.validationMessage         
     }else{        
         document.getElementById("output1").classList.remove("invisible")
         document.getElementById("output1").classList.add("visible")   
-        document.getElementById("alert").classList.remove("visible")
-        document.getElementById("alert").classList.add("invisible")
+        document.getElementById("alert1").classList.remove("visible")
+        document.getElementById("alert1").classList.add("invisible")
+        document.getElementById("alert2").classList.remove("visible")
+        document.getElementById("alert2").classList.add("invisible")
 
         if (count > 2) {
             document.getElementById("output1").innerHTML = `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${input1} ไม่ใช่จำนวนเฉพาะ เพราะ มี ${output1} เป็นตัวประกอบ`           
@@ -33,7 +37,48 @@ function cal(){
         }        
     }    
 }
+function cal2() {
+    const input2 = document.getElementById("input2").value   
+    let output2 = ""            
+    for(let i=2 ; i<=input2 ; i++){         
+        let count = 0      
+        for(let j=1 ; j<=i ; j++){
+            if (i%j == 0) {
+                count++                        
+            }            
+        }        
+        if(count <= 2){
+            if (i < input2) {
+                output2 = output2 + ` ${i} , `
+            } else {
+                output2 = output2 + ` ${i}`
+            }       
+        }
+    } 
+    const inpObj2 = document.getElementById("input2")
+    if (!inpObj2.checkValidity()) {
+        document.getElementById("alert1").classList.remove("visible")
+        document.getElementById("alert1").classList.add("invisible")
+        document.getElementById("alert2").classList.remove("invisible")
+        document.getElementById("alert2").classList.add("visible")
+        document.getElementById("output1").classList.remove("visible")
+        document.getElementById("output1").classList.add("invisible")   
+
+        document.getElementById("alert2").innerHTML = inpObj2.validationMessage         
+    }else{        
+        document.getElementById("output1").classList.remove("invisible")
+        document.getElementById("output1").classList.add("visible")   
+        document.getElementById("alert1").classList.remove("visible")
+        document.getElementById("alert1").classList.add("invisible")
+        document.getElementById("alert2").classList.remove("visible")
+        document.getElementById("alert2").classList.add("invisible")
+        
+        document.getElementById("output1").innerHTML = `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;จำนวนเฉพาะจาก 2 ถึง ${input2} มีดังนี้ ${output2}`           
+    }                
+}
 function resetPage() {
     // window.location.reload()
     setTimeout(function(){location.reload()}, 100)
 }
+
+
